ffffdd8e4d60c2417d18e4e841b9979b2fa7f9d2
Petr Panteleyev <petr-panteleyev@yandex.ru>
2024-11-15 20:44:10 +0300

    Java Commons 1.5.0
    * org.panteleyev.commons.crypto

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,8 @@
 module org.panteleyev.commons {
     exports org.panteleyev.commons.xml;
     exports org.panteleyev.commons.password;
+    exports org.panteleyev.commons.crypto;
 
     requires java.xml;
+    requires java.logging;
 }
